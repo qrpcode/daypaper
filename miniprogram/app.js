3743db8fc7fa143cc49d42edcf865d0436a31118
qiruipeng <i@qiruipeng.com>
2020-08-05 15:28:20 +0800

    📱微信小程序

---
+++ miniprogram/app.js
@@ -0,0 +1,40 @@
+// app.js 
+const ald = require('./utils/ald-stat.js')
+App({
+  onLaunch: function (options) {
+    const updateManager=wx.getUpdateManager(); 
+    updateManager.onCheckForUpdate(function(res){
+        if(res.hasUpdate){
+            updateManager.onUpdateReady(function(){
+                wx.showModal({
+                    title:'更新提示',
+                    content:'新版本已经准备好，点击确定重新启动',
+                    showCancel:false,
+                    success:res=>{
+                        if(res.confirm){
+                            updateManager.applyUpdate();
+                        }
+                    }
+                })
+            })
+            updateManager.onUpdateFailed(function(){
+                wx.showModal({
+                    title:'提示',
+                    content:'检查到有新版本，但是下载失败，请检查网络设置',
+                    showCancel:false
+                })
+            })
+        }
+    })
+  },
+  globalData: {
+    sendShow:false,
+    pddapi: "",
+
+    apiUrl: '',
+    //apiUrl: 'http://localhost:85/api/api.php',
+
+    fortuneNum: 0,
+    store: 1
+  }
+})
